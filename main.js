b58f7f9c5f91b4c3288c9ddcf3f7276c96ee0af5
Marco <marco.aristex89@gmail.com>
2023-03-25 13:02:03 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let remedios= [];
+
+function addRemedio(){
+event.preventDefault();
+
+let pet={
+    "remedioName": document.getElementById('fname').value,
+    "remedioQuantidade": document.getElementById('fquantidade').value,
+    "remedioPreco":document.getElementById('fpreco').value,
+    "remedioClasse":document.getElementById('fclasse').value,
+}
+}
